6d0c406ebd3ecab2037328d76cb592b6ebff1a00
Fernanda Lehaci <fernanda.lehaci@gmail.com>
2019-12-12 00:36:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function updateObjectWithKeyAndValue(object, key, value){
 
 }
-updateObjectWithKeyAndValue(object, key, value){
+function updateObjectWithKeyAndValue(object, key, value){
 
 }
-updateObjectWithKeyAndValue(object, key, value){
+function updateObjectWithKeyAndValue(object, key, value){
 
 }
-destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 }
